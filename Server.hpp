2fdd364ba97e5b7e41da32c612095d09fc8be131
kwillum <daniilxod@gmail.com>
2021-01-31 08:24:46 +0300

    add server description

--- Server.hpp
+++ Server.hpp
@@ -1,61 +1,96 @@
 #pragma once
 #include "includes.hpp"
-// #include <utility>
 #include <map>
 
-// typedef std::pair<std::string, std::string>	stringPair;
 typedef std::map<std::string, std::string>	stringMap;
 typedef std::map<std::string, std::string>::iterator	mapIter;
 
 class Server
 {
 private:
-    int                 _fdSocket;
-    unsigned int        _port;
-    std::string         _serverName;
-    struct sockaddr_in  _sockAddr;
-	std::string			_root;		//_locations enought to define server
-	stringMap			_locations; //correspondese location -> root ?
-    int                 createSocket();
-    void                 closeSocket();
-public:
-    const std::string   &getServerName() const;
-    unsigned int        getServePort() const;
-    int                 getServerSocket() const;
-    const std::string	getRoot() const;
-    void				setRoot(const std::string &root);
-	void				setServePort(unsigned int port);
-    void				setServerName(const std::string &sName);
-	void				appendLocation(const std::string &location, std::string const &root);
-	std::string			getUri(std::string const &location);
+	int					_socket;		/* file descriptor analog for network applications*/
+	unsigned int		_port;			/* ordinary network port http://blabla:port/ */
+	std::string			_serverName;	/* ip address of the listening server '127.0.0.1' by default */
+	struct sockaddr_in 	_sockAddr;		/* c/c++ structure for sockets */
+	std::string			_root;			/* root directory of the server. like in nginx configs */
+	stringMap			_locations;		/* also like in nginx. location to translate requset uri into directory on server */
 	
+	int					createSocket();	/* the main function for server creation. throw (const char *) an exception if the server failed to create */
+	void				closeSocket();
 	Server();
-    Server(std::string serverName, unsigned int port);
-    ~Server();
+public:
+			/* getters and setters */
+	const std::string	&getServerName() const;
+	unsigned int		getServePort() const;
+	int					getServerSocket() const;
+	const std::string	getRoot() const;
+	void				setRoot(const std::string &root);
+	void				setServePort(unsigned int port);
+	void				setServerName(const std::string &sName);
+
+			/* add new location to the server. may be useful when parsing config file */
+	void				appendLocation(const std::string &location,\
+										std::string const &root);
+	
+			/* funcrion to transform uri from http-requst into file-path on server */
+	std::string			getUri(std::string const &location);
+
+			/* coplien form */ 
+	Server	&operator=(const Server &s);
+	Server(std::string serverName, unsigned int port);
+	Server(const Server &s);
+	~Server();
 };
 
-Server::Server()
-{
-}
-
+Server::Server() {};
+Server::~Server() {};
+void				Server::closeSocket() {if (_socket != -1) close(_socket);};
+std::string const	&Server::getServerName() const {return (_serverName);}
+unsigned int		Server::getServePort() const {return (_port);};
+int					Server::getServerSocket() const {return (_socket);};
 void				Server::setRoot(const std::string &root){_root = root;};
 void				Server::setServePort(unsigned int port) {_port = port;};
-void				Server::setServerName(const std::string &sName) {_serverName = sName;}; 
+void				Server::setServerName(const std::string &sName) {_serverName = sName;};
 void				Server::appendLocation(const std::string &location, std::string const &root="")
 {
 	if (root == "")
 		_locations[location] = _root;
 	else
-		_locations[location] = root;		
+		_locations[location] = root;												/* must be carefull here. for correct working ROOT for server must be set first */
 }
 
-std::string			Server::getUri(std::string const &uri)
+int					Server::createSocket()
 {
+	int yes = 1;
+	if ((_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+		throw ("Socket creation error");
+	fcntl(_socket, F_SETFL, O_NONBLOCK);
+	if (setsockopt(_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1)		/* for reusing _socket with same id */
+		throw ("Socket options error.");
+	memset(&_sockAddr, 0, sizeof(_sockAddr));
+	_sockAddr.sin_family = AF_INET;
+	if  (_serverName == "localhost")
+		_sockAddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	else if (_serverName == "any")
+		_sockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	else if ((_sockAddr.sin_addr.s_addr = inet_addr(_serverName.c_str()))\
+				== INADDR_NONE)
+		throw ("Server addres error");
+	_sockAddr.sin_port = htons(_port);
+	if (bind(_socket, reinterpret_cast<sockaddr *>(&_sockAddr), sizeof(_sockAddr)))	/* set corresponding beteween socket and server ip:port */
+		throw ("Bind error.");
+	if (listen(_socket, 10))														/* open socket for listening and set max connections number for current server */
+		throw ("Listen failed.");
+	return (1);
+}
+
+std::string			Server::getUri(std::string const &uri)							/* Itrates by the location and find it as substring into uri. */
+{																					/* Remebver most longest corresponding */
 	mapIter	itLoc = _locations.begin();
 	mapIter	itBest = _locations.end();
 	size_t	pos = 0;
 	int		first = 1;
-	// std::string	filename = uri.substr(uri.find_last_of('/'));
+	// std::string	filename = uri.substr(uri.find_last_of('/'));					/* may be useful */
 	// size_t	pos = uri.find_last_of('/'); //possible case delim==npos?
 	// std::string	path = uri.substr(0, pos + 1);
 	while (itLoc != _locations.end())
@@ -74,78 +109,47 @@ std::string			Server::getUri(std::string const &uri)
 			itBest = itLoc;
 		itLoc++;
 	}
-	if (itBest == _locations.end())
+	if (itBest == _locations.end())													/* what path should return if location for such uri does not exist ? */
 		return ("/");
 	if (itBest->second == "/")
 		return (uri.substr(itBest->first.length()));
 	return (itBest->second + uri.substr(itBest->first.length()));
 }
 
-Server::~Server()
-{
-    // if (_fdSocket != -1)
-    //     close(_fdSocket);
-}
-
 Server::Server(std::string serverName, unsigned int port) try :
-    _serverName(serverName), _port(port)
+		_serverName(serverName), _port(port), _root("")
 {
-    createSocket();
+	createSocket();
 }
-catch(std::string const &s)
+catch(char const *s)
 {
-    std::cout << s << std::endl;
+	std::cout << s << std::endl;
 };
 
-void     Server::closeSocket()
+Server::Server(const Server &s) try :
+	_port(s._port), _serverName(s._serverName), _root(s._root),
+	_locations(s._locations), _socket(-1)
 {
-    if (_fdSocket != -1)
-        close(_fdSocket);
+	createSocket();
+}
+catch(const char *s)
+{
+	std::cout << s << std::endl;
 }
 
-int    Server::createSocket()
+Server	&Server::operator=(const Server &s)
 {
-	int yes = 1;
-	if ((_fdSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-		throw ("Socket creation error");
-	fcntl(_fdSocket, F_SETFL, O_NONBLOCK);
-	if (setsockopt(_fdSocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1)
-		throw "Socket options error.";
-	memset(&_sockAddr, 0, sizeof(_sockAddr));
-	_sockAddr.sin_family = AF_INET;
-	if  (_serverName == "localhost")
-		_sockAddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    else if (_serverName == "any")
-		_sockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    else if ((_sockAddr.sin_addr.s_addr = inet_addr(_serverName.c_str()))\
-				== INADDR_NONE)
-		throw ("Server addres error");
-	_sockAddr.sin_port = htons(_port);
-	if (bind(_fdSocket, reinterpret_cast<sockaddr *>(&_sockAddr), sizeof(_sockAddr)))
-		throw ("Bind error.");
-	if (listen(_fdSocket, 10))
-		throw ("Listen failed.");
-	return (1);
-}
-
-std::string const	&Server::getServerName() const
-{
-	return (_serverName);
-}
-
-unsigned int		Server::getServePort() const
-{
-	return (_port);
-}
-
-int					Server::getServerSocket() const
-{
-	return (_fdSocket);
+	_port = s._port;
+	_serverName = s._serverName;
+	_root = s._root;
+	_locations = s._locations;
+	closeSocket();
+	createSocket();																/* Exception may be thrown here */
 }
 
 std::ostream		&operator<<(std::ostream &o, const Server &s)
 {
-	o << "host: " << s.getServerName() << ", port: " << s.getServePort()\
-	<< ", socket: " << s.getServerSocket();
+	o << "host: " << s.getServerName() << ", port: " << \
+	s.getServePort() << ", socket: " << s.getServerSocket();
 	return (o);
 }
