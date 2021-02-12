2939d4e5ff655e8f749c166afe6ee876d710cb92
Olga K <okhuraskina@gmail.com>
2021-02-12 23:53:11 +0300

    reading from fd - sendBody

--- MethodGet.cpp
+++ MethodGet.cpp
@@ -1,60 +1,84 @@
 # include "MethodGet.hpp"
 
-// MethodGet::MethodGet(){};
-
-MethodGet::MethodGet(int socket, t_serv *config) : _socket(socket), _config(config), _status(200) {
+MethodGet::MethodGet(t_serv *config) : _config(config) {
 	// check_path_validity?
 };//200
 
 MethodGet::~MethodGet(){};
 
-std::string		*MethodGet::headerToString(OurHeader *buf)//contentToString?
-{
-};
+// std::string		*MethodGet::headerToString(OurHeader *buf)//contentToString?
+// {
+// };
 
-int		MethodGet::sendHeader() {
-	std::string *header = headerToString(_header);
+int		MethodGet::sendHeader(int socket) {
+	// std::string *header = headerToString(_header);
+	char header[] = "HTTP/1.1 200 OK\r\n"
+	"Server: nginx/1.2.1\r\n"
+	"Date: Sat, 08 Mar 2014 22:53:46 GMT\r\n"
+	"Content-Type: text/html\r\n"
+	"Content-Length: 58\r\n"
+	"Last-Modified: Sat, 08 Mar 2014 22:53:30 GMT\r\n\r\n";
 
 	// if (send(_socket, (const void *)header, header->size(), 0) < 0)//reinterpret
-	if (send(_socket, reinterpret_cast<const void *>(header), header->size(), 0) < 0)//reinterpret
+	// if (send(_socket, reinterpret_cast<const void *>(header), header->length(), 0) < 0)//reinterpret
+	if (send(socket, reinterpret_cast<const void *>(header), strlen(header), 0) < 0){//reinterpret
 		_status = errorSendHeader;
-	return -1;
+		return error;
+	}
+
+	return ok;
 };
 
 int		MethodGet::processRequest(std::string &path) {
 	struct stat	st;
 	if (stat(path.c_str(), &st) == -1){//lstat?
 		_status = notFound;
-		return -1;
+		return error;
 	}
 
-	std::ifstream body(path);
-	body.open(path.c_str());
-	if (!body.is_open()){
-		_status = errorOpeningURL;
-		return -1;
-	}
-// check_socket
+	// std::ifstream body(path);
+	// body.open(path.c_str());
+	// if (!body.is_open()){
+	// 	_status = errorOpeningURL;
+	// 	return error;
+	// }
 
-	if ((int fd = open(path.c_str(), O_RDONLY)) <= 0)
-
-	char	buf[BUFSIZE];
-	while (!body.eof()){
-		body.read(buf, sizeof(buf));
-		send(_socket, buf, BUFSIZE, 0);
-	}
-	body.close();
-	_status = 200;
-
-	return 0;
+	return ok;
 };
 
-// int		MethodGet::sendBody() {
-// 	char	buf[1024];
-// 	while (!infile.eof())
-// 	{
-// 		infile.read (buf, sizeof(buf));
-// 		Socket->write(buf,infile.gcount());
-// 	}
-// 	body.close();
-// }
+int		MethodGet::sendBody(std::string &path, int socket) {
+	// char	buf[1024];
+	// while (!infile.eof())
+	// {
+	// 	infile.read (buf, sizeof(buf));
+	// 	socket->write(buf,infile.gcount());
+	// }
+	// body.close();
+
+// check_socket
+	int fd;
+	if ((fd = open(path.c_str(), O_RDONLY)) <= 0){
+		_status = errorOpeningURL;
+		return error;
+	}
+
+	int		ret;
+	char	buf[BUFSIZE];
+
+	while (ret = read(fd, buf, BUFSIZE) > 0){// >= 0 //
+		int sent = send(socket, buf, ret, 0);
+		if (sent == 0)//
+			break ;
+		if (sent < 0){
+			_status = errorSocket;
+			return error;
+		}
+	}
+	if (ret == -1){
+		_status = errorReadingURL;
+		return error;
+	}
+	close(fd);
+	_status = 200;
+	return ok;
+}
--- MethodGet.hpp
+++ MethodGet.hpp
@@ -12,25 +12,24 @@
 
 typedef	enum
 {
-	noRequest,
-	noMethod,
-
-	errorCreateHeader = 1,
-	errorCreateErrorHeader = 1,
-	errorReadBody = 1,
-	errorProcessBody = 1,
+	// errorCreateHeader = 1,
+	// errorCreateErrorHeader = 1,
+	// errorReadBody = 1,
+	// errorProcessBody = 1,
 	notFound = 404,
 	errorOpeningURL = 403,//
+	errorReadingURL = 1,
+	errorSocket = 1,
 
 	errorSendHeader = 1,
 	okWaitingMoreChunks = 1,//222 wtf
 	ok = 200
 }		ErrorStatus;
 
-typedef struct	Header {
-	std::string header;
-	// std::map<string, string> {"header_name" : "value"}
-}				OurHeader;
+// typedef struct	Header {
+// 	std::string header;
+// 	// std::map<string, string> {"header_name" : "value"}
+// }				OurHeader;
 
 // std::string		generateHeader(std::string content, std::string path, int code)
 // {
@@ -40,65 +39,33 @@ typedef struct	Header {
 // 	return (header);
 // }
 
+//max body size
 
 
-class MethodGet : public IMethod
+class MethodGet : public AMethod
 {
 private:
-	int				_socket;
 	t_serv			*_config;
-	// std::string		_path;//
 	int				_status;
-
-	OurHeader		*_header;
-	// Response	*_response;//std::string
-	std::string		*_body;//
 
 	MethodGet(MethodGet & const src);
 	MethodGet & operator=(MethodGet & const src);
 	MethodGet();
 
 public:
-	// MethodGet(int socket, t_serv *config, std::string &path);//if status not ok don't call MethodGet in Response
-	MethodGet(int socket, t_serv *config);
+	MethodGet(t_serv *config);//if status not ok don't call MethodGet in Response
 	~MethodGet();
 
 	int	createHeader() {};
 	int	createErrorHeader() {};
-	int	sendHeader();
+	int	sendHeader(int socket);
 	int	sendError() {};
 
-	std::string	*headerToString(OurHeader *buf);// const
+	// std::string	*headerToString(OurHeader *buf);// const
 
-	int			getStatus() const { return this->_status; };//will be retrieved from Response
-	OurHeader	*getHeader() const { return this->_header; };// std::map	getHeader
-	std::string	*getBody() const { return this->_body; };
-
+	int		getStatus() const { return this->_status; };//will be retrieved from Response
 	int		processRequest(std::string &path);
-	int		sendBody();
-	// {
-	// 	// this->createHeader();//createErrorHeader()?//senError?
-	// 	// this->sendHeader();//when
-
-	// 	std::string content = "";
-	// 	std::string path = getPath();//путь из conf?
-
-	// 	struct stat		buf;
-	// 	if (stat(path.c_str(), &buf) == -1){
-	// 		std::cout << "Cannot open file.\n";//file does not exist
-	// 		return ;//error 202?404
-	// 	}
-
-	// 	std::ifstream	file;
-	// 	file.open(path.c_str(), std::ifstream::in);
-	// 	if (file.is_open() == false)
-	// 		return ;//403
-	// 	// content = readContent(path);
-		content.assign(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());///
-		file.close();
-
-		return (200);
-	}
+	int		sendBody(std::string &path, int socket);
 };
 
 // int		readContent(void);
