3b6e2dad2d76ba627bd5a86c89fd609fc2808ccd
abibi <andreytrue10@gmail.com>
2021-02-21 18:26:43 +0300

    method put

--- MethodPut.cpp
+++ MethodPut.cpp
@@ -1,11 +1,40 @@
 #include "MethodPut.hpp"
 
 MethodPut::~MethodPut() {};
-MethodStatus	MethodPut::createHeader(std::string const &_path) { return (ok); };
-MethodStatus	MethodPut::processBody(const std::string &requestBody) { return (ok); };
-MethodStatus	MethodPut::manageRequest(std::string const &path) { return (ok); };
-MethodStatus	MethodPut::sendBody(int socket) { return (ok); };
+MethodStatus	MethodPut::createHeader(std::string const &_path)
+{
+	return (ok);
+};
+
+MethodStatus	MethodPut::processBody(const std::string &requestBody)
+{
+	int size;
+	if (!_inProgress)
+	{
+		_uri.pathFd = open(_uri.path.c_str(), O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		if (_uri.pathFd < 0)
+			return (error); // TODO: send error or something
+		_inProgress = true;
+	}
+	size = write(_uri.pathFd, requestBody.c_str(), requestBody.length());
+	if (size < 0)
+		return (error); // TODO: can't write to file, send error
+	return (ok); // TODO: or in progress?
+};
+
+MethodStatus	MethodPut::manageRequest(std::string const &path)
+{
+	return (ok);
+};
+
+MethodStatus	MethodPut::sendBody(int socket)
+{
+	return (ok);
+};
+
 MethodStatus	MethodPut::sendHeader(int socket)
 {
+	_inProgress = false;
+	close(_uri.pathFd);
 	return (AMethod::sendHeader(socket));
 };
--- MethodPut.hpp
+++ MethodPut.hpp
@@ -6,14 +6,23 @@ class MethodPut: public AMethod
 {
 private:
     /* data */
+    struct uri
+	{
+		std::string path;
+		char **env;
+		int pathFd;
+		int socket;
+	};
+    uri _uri;
+    bool _inProgress;
 	MethodPut(/* args */);
 public:
 	MethodPut(t_serv const &config, int &status, stringMap const &headers) : 
-		AMethod(config, status, headers) {_type = PUT; };
-	~MethodPut();
-	MethodStatus	createHeader(std::string const &_path);
-	MethodStatus	processBody(const std::string &requestBody);
-	MethodStatus	sendHeader(int socket);
-	MethodStatus	manageRequest(std::string const &location);
-	MethodStatus	sendBody(int socket);
+		AMethod(config, status, headers) {_type = PUT; _inProgress = false; };
+	virtual ~MethodPut();
+	virtual MethodStatus	createHeader(std::string const &_path);
+	virtual MethodStatus	processBody(const std::string &requestBody);
+	virtual MethodStatus	sendHeader(int socket);
+	virtual MethodStatus	manageRequest(std::string const &location);
+	virtual MethodStatus	sendBody(int socket);
 };
