68cabd352092fe0ba6fbfe7565da619b64cf9d58
abibi <andreytrue10@gmail.com>
2021-02-22 17:10:09 +0300

    added fork, parse headers after cgi

--- CGI.cpp
+++ CGI.cpp
@@ -1,7 +1,64 @@
 #include "CGI.hpp"
 
-CGI::CGI(char **env)
+CGI::CGI(char *execpath, char **args, char **env)
 {
-	int pipein[2], pipeout[2];
+	// initialasing the pipe
+	pipe(pipein); // TODO: error handle
+	pipe(pipeout); // TODO: error handle
+	pid = fork(); // TODO: if pid < 0 error;
+	if (pid == 0)
+	{
+		dup2(pipein[1], 0);
+		close(pipein[1]);
+		dup2(pipeout[0], 1);
+		close(pipeout[0]);
+		execve(execpath, args, env);
+	}
+//	waitpid(pid, &status, WUNTRACED);
+}
 
+void CGI::cgiInput(const std::string &str)
+{
+	int r;
+	r = write(pipein[0], str.c_str(), str.length());
+}
+
+std::string &CGI::cgiOut()
+{
+	char buf[BUFSIZ];
+	std::string str;
+	size_t find;
+	int r;
+	r = read(pipeout[1], buf, BUFSIZ); // TODO: read > 0
+	str = buf;
+	if ((find = str.find("\n\n")) != std::string::npos) // headers ready for parser
+	{
+		parseHeaders(str.substr(0, find)); // todo: test how it works
+		str = str.substr(find + 2, str.size()); //
+		if (!str.empty())
+			std::cout << "body is ready!";
+		std::cout << "body is missing!";
+	}
+}
+
+CGI::~CGI()
+{
+
+}
+
+void CGI::parseHeaders(std::string str)
+{
+	size_t pos;
+	std::string key;
+	std::string value;
+	while (str.find('\n') != std::string::npos)
+	{
+		key = str.substr(0, str.find(':') - 1);
+		for (int i = 0; i < key.length(); i++)
+			key.at(i) = std::tolower(key.at(i));
+		value = str.substr(str.find(':'), str.find('\n'));
+		value = value.substr(value.find_first_not_of(" \v\t"), value.size()); // test ' '
+		str = str.substr(str.find('\n'), str.size());
+		_headersMap.insert(std::pair<std::string, std::string>(key, value));
+	}
 }
--- CGI.hpp
+++ CGI.hpp
@@ -1,23 +1,31 @@
 // Common Gateway Interface. Headers in script:
 // Content-Type - !MUST BE!
-// Content-length 
+// Content-length
 // Expires
 // Location	- Redirection, probably we do not handle this case
 // Pragma - * Turns document caching on and off.
 // Status (cannot be sent as part of a complete header).
-// class input: **env in constructor.
+// class constructor input: **env in constructor, args, execpath;
 // class methods: void cgiIn(const std::string &str); MethodStatus cgiOut(?);
-
+// TODO: args: arg[0] = executable file; arg[1] = location root; arg[2] = NULL
 #ifndef CGI_HPP
 #define CGI_HPP
-
+#include "include_resources.hpp"
+#include <sys/types.h>
+#include <sys/wait.h>
 
 class CGI
 {
 	private:
 		int pipein[2], pipeout[2]; // 2 pipes
+		int pid; // fork id
+		int status;
+		stringMap _headersMap;
+		void parseHeaders(std::string str);
 	public:
-		CGI(char **env); // prepare cgi process, prepare forks, etc
+		CGI(char *execpath, char **args, char **env); // prepare cgi process, prepare forks, etc
+		void cgiInput(const std::string &str); // ready to input;
+		std::string &cgiOut(); // ready to output
 		~CGI();
 };
 
