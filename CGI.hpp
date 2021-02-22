9bc4cf5949135f25acb84257c31bb1e9c96c060c
abibi <andreytrue10@gmail.com>
2021-02-22 14:14:37 +0300

    test commit

--- CGI.hpp
+++ CGI.hpp
@@ -1,12 +1,13 @@
 // Common Gateway Interface. Headers in script:
 // Content-Type - !MUST BE!
-// Content-length
+// Content-length 
 // Expires
 // Location	- Redirection, probably we do not handle this case
 // Pragma - * Turns document caching on and off.
 // Status (cannot be sent as part of a complete header).
 // class input: **env in constructor.
 // class methods: void cgiIn(const std::string &str); MethodStatus cgiOut(?);
+
 #ifndef CGI_HPP
 #define CGI_HPP
 
