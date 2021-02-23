6ce8fd742ff4ce0d47f50a9f9d2f407674044ed8
bshang <bshang@student.21-school.ru>
2021-02-24 00:15:21 +0300

    autoindex half on in methodGet

--- MethodGet.cpp
+++ MethodGet.cpp
@@ -13,12 +13,12 @@ int			MethodGet::generateIdxPage(std::string const &path){
 	struct dirent	*cur;
 
 	dir = opendir(path.c_str());//if -1
-	if (!dir){
+	if (!dir){//EACCES
 		_statusCode = errorOpeningURL;
 		return -1;//
 	}
 	_body = "<html>\n<body>\n"
-			"<h1>Directory listing</h1>\n";
+			"<h1>Directory listing:</h1>\n";
 	errno = 0;
 	while ((cur = readdir(dir)) != NULL && errno == 0){
 		if (cur->d_name[0] == '.')
@@ -30,6 +30,7 @@ int			MethodGet::generateIdxPage(std::string const &path){
 		_body += "\">";
 		_body += cur->d_name;
 		_body += "</a><br>\n";
+		// std::cout << "_body" << _body << std::endl;
 	}
 	closedir(dir);
 	_body += "</body>\n</html>\n";
@@ -39,14 +40,16 @@ int			MethodGet::generateIdxPage(std::string const &path){
 MethodStatus	MethodGet::manageRequest(std::string const &path)
 {
 	struct stat	st;
-	if (stat(path.c_str(), &st) == -1){// && errno == ENOENT)//lstat?//fstat IS_DIR
+	if (stat(path.c_str(), &st) == -1){// && errno == ENOENT)
 		_statusCode = notFound;
 		return error;
 	}
-	// if (S_ISDIR(st.st_mode) && _config.locs[0].autoindex)
-	// 	generateIdxPage(path);//signalize that body not from fd
-	// else
-	if ((_fd = open(path.c_str(), O_RDONLY | O_NONBLOCK)) <= 0){//else
+	if (S_ISDIR(st.st_mode) && _config.locs[0].autoindex){
+		if (generateIdxPage(path))//signalize that body not from fd
+			return error;
+	}
+	// else S_ISFIFO S_ISLNK /// O_DIRECTORY
+	else if (S_ISREG(st.st_mode) && (_fd = open(path.c_str(), O_RDONLY | O_NONBLOCK)) < 0){
 		_statusCode = errorOpeningURL;
 		return error;
 	}
@@ -120,33 +123,39 @@ MethodStatus		MethodGet::sendResponse(int socket)
 
 	memset(buf, 0, _bs);
 	if (_statusCode != okSendingInProgress){
-		_remainder.clear();
 		_header->headersToString(_headersMap, _statusCode, response);
 		size_t headersize = response.length();
 
-		// if (!_body.empty()){
-		// 	_bytesToSend = _body.length() + headersize;
-		// }
-		// else {
+		if (!_body.empty()){//only for autoindex
+			_bytesToSend = _body.length() + headersize - 1;//-1
+			response += _body;
+			size_t pos = response.find("Content-Length", 0);
+			pos += 16;
+			size_t fin = response.find_first_of(CRLF, pos);
+			response.replace(pos, fin - pos, std::to_string(_body.length()));//to_string
+		}
+		else {
 			struct stat sbuf;
 			fstat(_fd, &sbuf);
 			_bytesToSend = sbuf.st_size + headersize;
 			readBuf -= headersize;
-		// }
+		}
 	}
 	if (!_remainder.empty()){
 		readBuf = _bs - _remainder.length();
 		response = _remainder;
 	}
-	readBytes = read(_fd, buf, readBuf);
-	if (readBytes < 0){
-		_statusCode = errorReadingURL;
-		close(_fd);
-		return error;
+	if (_body.empty()){//not listing -> //no if braces without autoindex
+		readBytes = read(_fd, buf, readBuf);
+		if (readBytes < 0){
+			_statusCode = errorReadingURL;
+			close(_fd);
+			return error;
+		}
+		buf[readBuf] = '\0';
+		std::string bufStr(buf, readBytes);
+		response += bufStr;
 	}
-	buf[readBuf] = '\0';
-	std::string bufStr(buf, readBytes);
-	response += bufStr;
 
 	sentBytes = send(socket, response.c_str(), response.length(), MSG_DONTWAIT);
 	if (sentBytes < 0 || errno == EMSGSIZE){
@@ -165,6 +174,7 @@ MethodStatus		MethodGet::sendResponse(int socket)
 	};
 	// _remainder.clear();//
 	_sentBytesTotal += sentBytes;
+	std::cout << "_bytesToSend" << _bytesToSend << " _sentBytesTotal" << _sentBytesTotal << std::endl;
 	if (_sentBytesTotal < _bytesToSend){
 		_statusCode = okSendingInProgress;
 		return inprogress;
